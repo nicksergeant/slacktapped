1fa67228a2fc47e77ecf6a61547429e404336a24
Nick Sergeant <nick@nicksergeant.com>
2016-08-29 20:49:09 -0400

    Some work on basic functions.

--- lib/slacktappdex.ex
+++ lib/slacktappdex.ex
@@ -23,12 +23,74 @@ defmodule Slacktappdex do
     "badge"
   end
 
+  @doc """
+  Parses a checkin map to a string for Slack.
+
+  ## Examples
+
+      iex> checkin = %{
+      iex>   "user" => %{
+      iex>     "user_name" => "nicksergeant"
+      iex>   }
+      iex> }
+      iex> Slacktappdex.parse_checkin(checkin)
+      "nicksergeant is drinking"
+
+  """
   def parse_checkin(checkin) do
-    "checkin"
+    # TODO: If checkin description has #shh in it, ignore.
+    # TODO: Post image if we haven't already, even if checkin already.
+
+    username = get_in(checkin, ["user", "user_name"])
+
+    # """
+    #   #{username} is drinking #{beer_name} (#{beer_type}, #{abv} ABV)
+    #   by #{brewery}. They rated it a #{rating} and said \"#{comment}\".
+    # """
+    "#{username} is drinking"
   end
 
   def parse_comment(comment) do
     "comment"
+  end
+
+  @doc """
+  Returns the user's name. If both first and last name are present, it returns
+  "First-name Last-name", otherwise it returns "username".
+
+  ## Examples
+
+      iex> user = %{
+      iex>   "user_name" => "nicksergeant"
+      iex> }
+      iex> Slacktappdex.parse_username(user)
+      "nicksergeant"
+
+      iex> user = %{
+      iex>   "user_name" => "nicksergeant",
+      iex>   "first_name" => "Nick",
+      iex>   "last_name" => "Sergeant"
+      iex> }
+      iex> Slacktappdex.parse_username(user)
+      "Nick Sergeant"
+
+      iex> user = %{
+      iex>   "user_name" => "nicksergeant",
+      iex>   "first_name" => "Nick"
+      iex> }
+      iex> Slacktappdex.parse_username(user)
+      "nicksergeant"
+
+  """
+  def parse_username(user) do
+    case user do
+      %{
+        "first_name" => first_name,
+        "last_name" => last_name
+      } ->
+        "#{first_name} #{last_name}"
+      _ -> "#{user["user_name"]}"
+    end
   end
 
   defp process_badge(badge) do
--- test/slacktappdex_test.exs
+++ test/slacktappdex_test.exs
@@ -1,32 +1,13 @@
 defmodule SlacktappdexTest do
   use ExUnit.Case, async: true
+
   doctest Slacktappdex
 
-  setup do
-    %{
-      checkin_with_badge: Slacktappdex.checkins |> Enum.at(6),
-      checkin_with_comment: Slacktappdex.checkins |> Enum.at(0)
-    }
-  end
-
-  test "parse_badge", context do
-    badge = get_in(context.checkin_with_badge, ["badges", "items"])
-      |> Enum.at(0)
-
-    parsed = Slacktappdex.parse_badge(badge)
-    assert(parsed == "badge")
-  end
-
   test "parse_checkin", context do
-    parsed = Slacktappdex.parse_checkin(context.checkin_with_badge)
-    assert(parsed == "checkin")
-  end
+    # checkin = %{
 
-  test "parse_comment", context do
-    comment = get_in(context.checkin_with_comment, ["comments", "items"])
-      |> Enum.at(0)
+    # }
 
-    parsed = Slacktappdex.parse_comment(comment)
-    assert(parsed == "comment")
+    # assert(parsed == "checkin")
   end
 end
