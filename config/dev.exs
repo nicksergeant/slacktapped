ff467d33bdf60bc41ce3c71cc47e58a9cfa13f03
Nick Sergeant <nick@nicksergeant.com>
2016-08-31 22:27:51 -0400

    Writin' the codes. Takin' the names.

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,5 @@
 use Mix.Config
 
 config :mix_test_watch, clear: true
-config :slacktapped, :slack, Slacktapped.Slack.Mock
+config :slacktapped, :slack, Slacktapped.Slack.Live
 config :slacktapped, :untappd, Slacktapped.Untappd.Mock
--- lib/slacktapped.ex
+++ lib/slacktapped.ex
@@ -2,21 +2,61 @@ defmodule Slacktapped do
   @slack Application.get_env(:slacktapped, :slack)
   @untappd Application.get_env(:slacktapped, :untappd)
 
+  @doc """
+  Fetches checkins from Untappd and then processes each checkin, its badges,
+  and its comments.
+
+  ## Example
+
+     iex> Slacktapped.main
+     :ok
+
+  """
   def main do
     checkins
+      # |> Enum.take(1) # TODO: Remove for prod.
       |> Enum.each(fn(checkin) ->
-        checkin
-          |> process_checkin
-          |> process_badges
-          |> process_comments
+        with {:ok, checkin} <- process_checkin(checkin),
+             {:ok, checkin} <- process_badges(checkin),
+             {:ok, checkin} <- process_comments(checkin),
+             do: :ok
       end)
   end
 
+  @doc """
+  Makes an API request to Untappd to fetch the recent checkins, then gets the
+  body of the response. We then encode the response to a map and retrieve only
+  the checkin items.
+  """
   def checkins do
     @untappd.get("checkin/recent")
      |> Map.fetch!(:body)
      |> Poison.decode!
      |> get_in(["response", "checkins", "items"])
+  end
+
+  @doc """
+  Determines if a checkin is eligible to be posted to Slack. Checkin is
+  ineligible if the checkin_comment contains the text "#shh".
+
+  ## Examples
+
+      iex> Slacktapped.is_eligible_checkin(%{})
+      {:ok, %{}}
+
+      iex> Slacktapped.is_eligible_checkin(%{"checkin_comment" => "#shh"})
+      {:error, %{"checkin_comment" => "#shh"}}
+
+  """
+  def is_eligible_checkin(checkin) do
+    cond do
+      is_nil(checkin["checkin_comment"]) ->
+        {:ok, checkin}
+      String.match?(checkin["checkin_comment"], ~r/#shh/) ->
+        {:error, checkin}
+      true ->
+        {:ok, checkin}
+    end
   end
 
   def parse_badge(badge) do
@@ -36,14 +76,23 @@ defmodule Slacktapped do
       ...>   },
       ...>   "beer" => %{
       ...>     "bid" => 123,
+      ...>     "beer_abv" => 4.5,
       ...>     "beer_name" => "IPA",
-      ...>     "beer_slug" => "two-lake-ipa"
+      ...>     "beer_slug" => "two-lake-ipa",
+      ...>     "beer_style" => "American IPA"
+      ...>   },
+      ...>   "brewery" => %{
+      ...>     "brewery_id" => 1,
+      ...>     "brewery_name" => "Two Lake"
       ...>   }
       ...> })
       {:ok,
-        "<a href=\"https://untappd.com/user/nicksergeant\">nicksergeant</a> " <>
+        "<https://untappd.com/user/nicksergeant|nicksergeant> " <>
         "is drinking " <>
-        "<a href=\"https://untappd.com/b/two-lake-ipa/123\">IPA</a>"}
+        "<https://untappd.com/b/two-lake-ipa/123|IPA> " <>
+        "(American IPA, 4.5% ABV) " <>
+        "by <https://untappd.com/brewery/1|Two Lake>."
+      }
 
   A checkin without a rating:
 
@@ -54,21 +103,25 @@ defmodule Slacktapped do
   """
   def parse_checkin(checkin) do
     {:ok, user_name} = parse_name(checkin["user"])
-    user_username = checkin["user"]["user_name"]
-    user = "<a href=\"https://untappd.com/user/#{user_username}\">" <>
-      "#{user_name}</a>"
 
+    beer_abv = checkin["beer"]["beer_abv"]
     beer_id = checkin["beer"]["bid"]
     beer_name = checkin["beer"]["beer_name"]
     beer_slug = checkin["beer"]["beer_slug"]
-    beer = "<a href=\"https://untappd.com/b/#{beer_slug}/#{beer_id}\">" <>
-      "#{beer_name}</a>"
+    beer_style = checkin["beer"]["beer_style"]
+    brewery_id = checkin["brewery"]["brewery_id"]
+    brewery_name = checkin["brewery"]["brewery_name"]
+    user_username = checkin["user"]["user_name"]
 
-    # """
-    #   #{username} is drinking #{beer_name} (#{beer_type}, #{abv} ABV)
-    #   by #{brewery}. They rated it a #{rating} and said \"#{comment}\".
-    # """
-    {:ok, "#{user} is drinking #{beer}"}
+    beer = "<https://untappd.com/b/#{beer_slug}/#{beer_id}|#{beer_name}>"
+    brewery = "<https://untappd.com/brewery/#{brewery_id}|#{brewery_name}>"
+    user = "<https://untappd.com/user/#{user_username}|#{user_name}>"
+
+    # TODO: user rating and checkin_comment.
+
+    {:ok,
+      "#{user} is drinking #{beer} (#{beer_style}, #{beer_abv}% ABV) " <>
+      "by #{brewery}."}
   end
 
   def parse_comment(comment) do
@@ -112,33 +165,16 @@ defmodule Slacktapped do
     end
   end
 
-  @doc """
-  Determines if a checkin is eligible to be posted to Slack. Checkin is
-  ineligible if the checkin_comment contains the text "#shh".
-
-  ## Examples
-
-      iex> Slacktapped.is_eligible_checkin(%{})
-      {:ok, %{}}
-
-      iex> Slacktapped.is_eligible_checkin(%{"checkin_comment" => "#shh"})
-      {:error, %{"checkin_comment" => "#shh"}}
-
-  """
-  def is_eligible_checkin(checkin) do
-    cond do
-      is_nil(checkin["checkin_comment"]) ->
-        {:ok, checkin}
-      String.match?(checkin["checkin_comment"], ~r/#shh/) ->
-        {:error, checkin}
-      true ->
-        {:ok, checkin}
-    end
+  def process_badge(badge) do
+    parse_badge(badge)
+      |> @slack.post
+    {:ok, badge}
   end
 
-  def process_badge(badge) do
-    parse_badge(badge) |> @slack.post
-    {:ok, badge}
+  def process_badges(checkin) do
+    get_in(checkin, ["badges", "items"])
+      |> Enum.each(&(process_badge(&1)))
+    {:ok, checkin}
   end
 
   @doc ~S"""
@@ -147,14 +183,12 @@ defmodule Slacktapped do
   Ineligible checkins contain the text "#shh" in the checkin comment. For, you
   know, day drinking.
 
-  ## Examples:
+  ## Examples
 
   A normal checkin gets through:
 
       iex> Slacktapped.process_checkin(%{})
-      {:ok,
-        "<a href=\"https://untappd.com/user/\"></a> is drinking " <>
-        "<a href=\"https://untappd.com/b//\"></a>"}
+      {:ok, %{}}
 
   Checkins with the text "#shh" in the checkin comment are ignored:
 
@@ -164,24 +198,21 @@ defmodule Slacktapped do
   """
   def process_checkin(checkin) do
     with {:ok, checkin} <- is_eligible_checkin(checkin),
-         {:ok, checkin} <- parse_checkin(checkin),
-         do: @slack.post(checkin)
+         {:ok, message} <- parse_checkin(checkin),
+         {:ok, message} <- @slack.post(message),
+         do: {:ok, checkin}
   end
 
   def process_comment(comment) do
-    parse_comment(comment) |> @slack.post
+    parse_comment(comment)
+      |> @slack.post
     {:ok, comment}
   end
 
-  def process_badges({:ok, checkin}) do
-    get_in(checkin, ["badges", "items"])
-      |> Enum.each(&(process_badge(&1)))
-    {:ok, checkin}
-  end
-
-  def process_comments({:ok, checkin}) do
+  def process_comments(checkin) do
     get_in(checkin, ["comments", "items"])
       |> Enum.each(&(process_comment(&1)))
     {:ok, checkin}
   end
+
 end
