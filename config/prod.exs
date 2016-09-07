69c1c64308528e989bde4fc0ae1b4d7c360cfb0d
Nick Sergeant <nick@nicksergeant.com>
2016-09-07 09:31:34 -0400

    Debugging deploy.

--- config/prod.exs
+++ config/prod.exs
@@ -1,8 +1,12 @@
 use Mix.Config
 
-# {port, _} = Integer.parse(System.get_env("PORT"))
+IO.puts "============="
+IO.puts System.get_env("PORT")
+IO.puts "============="
+
+{port, _} = Integer.parse(System.get_env("PORT"))
 
 config :slacktapped,
-  cowboy_port: 80,
+  cowboy_port: 5000,
   slack: Slacktapped.Slack.Live,
   untappd: Slacktapped.Untappd.Live
