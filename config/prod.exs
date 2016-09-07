d639629d6d45ae5e508d72b6b8874b9eca0ceb61
Nick Sergeant <nick@nicksergeant.com>
2016-09-07 09:08:42 -0400

    Debugging deploy.

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
 config :slacktapped,
-  cowboy_port: System.get_env("PORT"),
+  cowboy_port: System.get_env("$PORT"),
   slack: Slacktapped.Slack.Live,
   untappd: Slacktapped.Untappd.Live
