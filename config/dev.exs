1b6bfd9f81b28d2c0a2830267950513744e70da5
Nick Sergeant <nick@nicksergeant.com>
2016-12-07 16:42:19 -0500

    Ports

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,6 @@ use Mix.Config
 config :mix_test_watch, clear: true
 config :slacktapped,
   beersearch: Slacktapped.BeerSearch.Mock,
-  cowboy_port: 8000,
   instance_name: "dev",
   redis: Slacktapped.Redis.Mock,
   redis_port: 6379,
--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,6 @@ config :quantum, cron: [
 
 config :slacktapped,
   beersearch: Slacktapped.BeerSearch.Live,
-  cowboy_port: 5000,
   redis: Slacktapped.Redis.Live,
   redis_port: 6379,
   slack: Slacktapped.Slack.Live,
