85436ea8b42c53b88a50d8b4a1803c0543437612
Nick Sergeant <nsergeant@hubspot.com>
2018-09-29 22:05:49 -0400

    Try and upgrade the things.

--- mix.exs
+++ mix.exs
@@ -36,14 +36,14 @@ defmodule Slacktapped.Mixfile do
   defp deps do
     [
       {:beersearch, "~> 0.0.8"},
-      {:cowboy, "~> 1.0.0"},
+      {:cowboy, "~> 2.4.0"},
       {:ex_doc, "~> 0.12", only: :dev},
       {:httpotion, "~> 3.0.0"},
       {:mix_test_watch, "~> 0.2", only: :dev},
       {:plug, "~> 1.0"},
-      {:poison, "~> 2.0"},
-      {:redix, ">= 0.4.0"},
-      {:quantum, ">= 1.7.1"}
+      {:poison, "~> 4.0.1"},
+      {:redix, ">= 0.8.1"},
+      {:quantum, "~> 2.3"}
     ]
   end
 end
